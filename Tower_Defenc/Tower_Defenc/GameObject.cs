78428d3896ebe55496b0b63e702fa6adc517ae7d
Лев <l_saltovskiy@mail.ru>
2021-08-17 19:02:01 +0300

    новая заливка

--- Tower_Defenc/Tower_Defenc/GameObject.cs
+++ Tower_Defenc/Tower_Defenc/GameObject.cs
@@ -19,21 +19,26 @@ namespace Tower_Defenc
 
         // если SubdivisionNumber = 0, НАШ.
         // если SubdivisionNumber = 1, ВРАГ самаходка LOW.
-        int state = 0;
-        public int SubdivisionNumber; 
+        Int64 state = 0;
+        int MaxCounter;
+        static int counter = 0;
+        public bool NeedToRotate = false;
+        public int SubdivisionNumber;
         public int TargetRadiusEnemy = 700;
         public int Range = 5;
         int DamageCounter = 2000;
         public double X;
         public double Y;
         public int HP;
-        public bool IsMarked = false;       
+        public bool IsMarked = false;
         double SpeedX;
         double SpeedY;
+        public int mode; // 0 - Обьект стоит на месте   // 1 - Обьект движется в точку на карте // 2 - Обьект движется к другому игровому обьекту;
+        public GameObject TargetObject;
         Rectangle HPLINE = new Rectangle();
         public bool NeedToMove = false;
-        public int TargetX;
-        public int TargetY;
+        //public int TargetX;
+        //public int TargetY;
         public int Angle;
         public double Speed;
         public string Type;
@@ -49,24 +54,49 @@ namespace Tower_Defenc
             map.Library.AddContainer(ContainerName + "top", "nothing", ContainerType.AutosizedSingleImage);
             map.Library.AddContainer(ContainerName + "mark", "AllyScope", ContainerType.AutosizedSingleImage);
             map.Library.AddContainer(ContainerName + "Anime", "nothing", ContainerType.AutosizedSingleImage);
-            Type = type;
+            Type = type; 
             HPLINE.Height = 10;
             HPLINE.Fill = Brushes.Green;
             map.Canvas.Children.Add(HPLINE);
-            map.ContainerSetZIndex(ContainerName + "Anime",86);
+            map.ContainerSetZIndex(ContainerName + "Anime", 86);
             //map.DrawRectangle((int)X - 35, (int)Y + 25, hp, 10, Brushes.Green, Brushes.Green);
+        }
+
+        public GameObject()
+        {
+            ////
         }
         public GameObject(string PICTURE, string Container, string type, int x, int y, int size): this(PICTURE, Container, type)
         {
-            map.ContainerSetMaxSide(ContainerName, 70);
+            map.ContainerSetMaxSide(ContainerName, size);
             SetCoordinate(x, y);
             map.ContainerSetAngle(ContainerName, 180);
             map.ContainerSetZIndex(ContainerName, 87);
         }
 
+        public void CheckObject()
+        {
+            CheckMaxCounter();
+        }
+
+        public void CheckMaxCounter()
+        {
+            if(MaxCounter >= 2000)  
+            {
+                MaxCounter = -900000000;
+                map.ContainerSetFrame(ContainerName + "mark", "nothing");
+            }
+            else
+            {
+                MaxCounter = MaxCounter + 10;
+            }
+        }
+
         public void SetCoordinate(double x, double y)
         {
             map.ContainerMovePreview(ContainerName, x, y, Angle);
+            // Этот цикл должен быть в отдельной функции проверки столкновения обьектов.
+            // /та функция вызывается в GameCycle.
             for (int i = 0; i < MainWindow.obstacle.Count; i++) 
             {
                 if (map.CollisionContainers(ContainerName, MainWindow.obstacle[i].ContainerName, true) &&
@@ -123,11 +153,21 @@ namespace Tower_Defenc
                 {
                     MainWindow.SelectedUnit.IsMarked = false;
                     map.ContainerSetCoordinate(MainWindow.SelectedUnit.ContainerName + "mark", -1000, -1000);
-                    //MainWindow.SelectedUnit.HPLINE.Height = 0;
+                    //MainWindow.SelectedUnit.HPLINE.Height = 0;99
                 }
                 MainWindow.SelectedUnit = this;
                 IsMarked = true;
                 map.ContainerSetMaxSide(ContainerName + "mark", 62);
+            }
+            else
+            {
+                map.ContainerSetFrame(ContainerName + "mark", "MarkEnemy");
+                map.ContainerSetMaxSide(ContainerName + "mark", 70);
+                IsMarked = true;
+                MaxCounter = 0;
+                MainWindow.SelectedEnemyUnit = this;
+                MainWindow.ClickCount = 5;
+                MainWindow.SelectedUnit.mode = 2;
             }
         }
 
@@ -136,7 +176,55 @@ namespace Tower_Defenc
             if(NeedToMove)
             {
                 SetCoordinate(X + SpeedX, Y + SpeedY);
-                double TargetAngle = GameMath.GetAngleOfVector(TargetX - X, TargetY - Y);
+                if (Math.Abs(X - TargetObject.X) < Range && Math.Abs(Y - TargetObject.Y) < Range)
+                {
+                    NeedToMove = false;
+                    NeedToRotate = false;
+                }
+            }
+            // Debug.WriteLine("{0}, {1}", X, Y);
+        }
+
+        bool CheckAim()
+        {
+            bool IsAimed = false;
+            double AngleToVector = GameMath.GetAngleOfVector(TargetObject.X - X, TargetObject.Y - Y);
+            if(Math.Abs(Angle - AngleToVector) < 3)
+            {
+                IsAimed = true;
+            }
+
+            return IsAimed;
+        }
+
+        public void PerformAction()
+        {
+            if(mode == 2 && CheckAim())
+            {
+                // выполнение выстрела.                                 
+                // Мы дожны созать переменную типа GameObject.           
+                // Координаты и угол как у игрокау этого GameObject(а). 
+                // Картинка снаряда взовисемонсти от того кто стрелял.   
+                // Добавить переменную к списку снарядов.                 
+                GameObject Shell = new GameObject("BulletAllies", "Bullet" + counter.ToString(), "BulletAllies");
+                counter++;
+                // задать координаты, угол поворота и установить скорость движение снаряда.
+                /*Shell.X = X;
+                Shell.Y = Y;
+                Shell.Angle = Angle;*/
+                Shell.SetCoordinate(X, Y);
+                Shell.SetAngle(Angle);
+                // задаём размер контейнера.
+                map.ContainerSetSize(Shell.ContainerName, 12);
+                // отрисовываем контейнер с нужным углом;
+            }
+        }
+
+        public void Rotate()
+        {
+            if (NeedToRotate)
+            {
+                double TargetAngle = GameMath.GetAngleOfVector(TargetObject.X - X, TargetObject.Y - Y);
                 if (TargetAngle > Angle)
                 {
                     SetAngle(Angle + 2);
@@ -144,10 +232,6 @@ namespace Tower_Defenc
                 else
                 {
                     SetAngle(Angle - 2);
-                }
-                if(Math.Abs(X - TargetX) < Range && Math.Abs(Y - TargetY) < Range)
-                {
-                    NeedToMove = false;
                 }
             }
         }
@@ -183,6 +267,7 @@ namespace Tower_Defenc
                 map.ContainerSetFrame(ContainerName, "Destroyed_Tank_Low_ALLY");
                 map.ContainerSetFrame(ContainerName + "mark", "nothing");
                 NeedToMove = false;
+                NeedToRotate = false;
                 map.AnimationStop(ContainerName + "Anime", "Fire1");
                 map.ContainerSetFrame(ContainerName + "Anime", "nothing");
             }
--- Tower_Defenc/Tower_Defenc/MainWindow.xaml.cs
+++ Tower_Defenc/Tower_Defenc/MainWindow.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Text; 
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,18 +22,25 @@ namespace Tower_Defenc
     /// </summary>
     public partial class MainWindow : Window
     {
+        int CenterX, CenterY;
         Random r = new Random();
         TimerController timer = new TimerController();
         UniversalMap_Wpf map;
+        static public GameObject SelectedEnemyUnit;
         static public List<GameObject> obstacle = new List<GameObject>();
         SimpleTextBox TextTimer = new SimpleTextBox();
         static public GameObject SelectedUnit; // Переменная для хранения выбранного юнита.
-        CellMapInfo cellMap = new CellMapInfo(31, 21, 50, 0);
+        CellMapInfo cellMap = new CellMapInfo(100, 100, 50, 0);
         InventoryPanel ipan;
+        public static int ClickCount = 0;
+        int scrollX;
+        int scrollY;
         GameObject basa;
         bool CanSpawn = true;
         List<GameObject> Enemis = new List<GameObject>();
         static public List<GameObject> Allies = new List<GameObject>();
+        List<GameObject> AlliesShots = new List<GameObject>();
+        List<GameObject> EnemisShots = new List<GameObject>();
         List<GameObject> CreatedAnimation = new List<GameObject>();
         Wave[] waves = new Wave[1]; 
         InventoryPanel UnitsPanel;
@@ -46,6 +53,11 @@ namespace Tower_Defenc
         {
             InitializeComponent();
             map = MapCreator.GetUniversalMap(this, cellMap);
+            CenterX = (int)map.Canvas.Width / 2;
+            CenterY = (int)map.Canvas.Height / 2;
+            scrollX = -CenterX + 800;
+            scrollY = -CenterY + 400;
+            map.Canvas.Margin = new Thickness(scrollX, scrollY, 0, 0);
             ipan = new InventoryPanel(map.Library, 40, 16);
             UnitsPanel = new InventoryPanel(map.Library, 120, 16);
             lay = LayoutsFactory.GetLayout(LayoutType.Vertical, this.Content);
@@ -56,12 +68,13 @@ namespace Tower_Defenc
             lay.Attach(TextTimer, 1);
             AddPictures();
             ipan.AddItem("money", "money", money.ToString());
-            UnitsPanel.AddItem("ЧИСТОТАНК", "Tank_Low_AllY", "Средний танк без поворота башни: 50");
+            UnitsPanel.AddItem("ЧИСТОТАНК", "Tank_Low_AllY", "Средний танк без поворота башни: 50. ИМЯ: ЖАР");
             UnitsPanel.SetMouseClickHandler(BuildUnit);
+            UnitsPanel.AddItem("ПУЛЕМЁТНИК", "TankMashingan_Medium_ALLY", "Малый танк без поворота башни: 100. ИМЯ: БАХ");
             GameObject.map = map;
             basa = new GameObject("basa", "BASA", "basa");
             map.ContainerSetSize(basa.ContainerName, 100, 100);
-            basa.SetCoordinate(map.XAbsolute / 2, map.YAbsolute / 2);
+            basa.SetCoordinate(CenterX, CenterY);
             timer.AddAction(GameCycle, 20);
             timer.AddAction(Countdown, 1000);
             TextTimer.TextBox.IsEnabled = false;
@@ -70,9 +83,6 @@ namespace Tower_Defenc
             timer.AddAction(CheckScroll, 12);
             //map.Keyboard.SetSingleKeyEventHandler(CheckKey);
         }
-
-        int scrollX = 0;
-        int scrollY = 0;
 
         void CheckScroll()
         {
@@ -108,12 +118,16 @@ namespace Tower_Defenc
             map.Library.AddPicture("money", "money.png");
             map.Library.AddPicture("nothing", "nothing.png");
             map.Library.AddPicture("Tank_Low_AllY", "Танк с башней без поворота(ДОБРЫЙ средний).png");
+            map.Library.AddPicture("BulletAllies", "СнарядДобрый.png");
             map.Library.AddPicture("basa", "base.png");
             map.Library.AddPicture("snow", "SONOW.png");
+            map.Library.AddPicture("TankMashingan_Medium_ALLY", "Танк с башней без поворота(ДОБРЫЙ СЛАБЫЙПУЛЕМЁТНЫЙ).png");
+            map.Library.AddPicture("MarkEnemy", "EnemyDetected.png");
+            map.Library.AddPicture("TankMashingan_Medium_ALLY_destroid", "Танк подорвали(второй).png");
             map.Library.AddPicture("AllyScope", "Выбрал нашего.png");
             map.Library.AddPicture("EnemyLOW", "Враг(самаходка).png");
             map.Library.AddPicture("Destroyed_Tank_Low_ALLY", "Танк подорвали(первый).png");
-            //map.SetMapBackground("background");
+            map.SetMapBackground("background");
             AddSetPictures("Fire Bolt", 6);
             AddSetPictures("ДЫМ", 4);
             AddSetPictures("exp", 10);
@@ -127,13 +141,13 @@ namespace Tower_Defenc
             /////////////////////////////////////////////////////// у
             /////////////////////////////////////////////////////// р
             /////////////////////////////////////////////////////// а
-            map.Library.AddContainer("TextureUp", "snow", ContainerType.SingleImage);
-            map.ContainerSetSize("TextureUp", 872, 598);
-            map.ContainerSetCoordinate("TextureUp", 0, 0);
-
-            map.Library.AddContainer("MainTexture", "background", ContainerType.SingleImage);
-            map.ContainerSetSize("MainTexture", 400, 400);
-            map.ContainerSetCoordinate("MainTexture", 0, 0);
+            map.Library.AddContainer("TextureUp", "snow", ContainerType.AutosizedSingleImage);
+            //map.ContainerSetSize("TextureUp", 5000, 1000);
+            map.ContainerSetMaxSide("TextureUp", 2000 );
+            map.ContainerSetCoordinate("TextureUp", 2500, 1000);
+            //map.Library.AddContainer("MainTexture", "background", ContainerType.SingleImage);
+            //map.ContainerSetSize("MainTexture", 400, 400);
+            //map.ContainerSetCoordinate("MainTexture", 0, 0);
         }
 
         void CreateWaves()
@@ -149,7 +163,7 @@ namespace Tower_Defenc
         void StartWave(int number)
         {
             for(int i = 0; i < waves[number].Units.Count; i++)
-            {
+                {
                 switch (waves[number].Units[i].UnitName)
                 {
                     case "EnemyLOW":
@@ -166,16 +180,17 @@ namespace Tower_Defenc
             {
                 counter++;
                 GameObject Enemy = new GameObject("EnemyLOW", "Enemy1" + counter.ToString(), "EnemyLOW");
-                int x = 1, y = 1;
-                Enemy.TargetX = map.XAbsolute / 2;
-                Enemy.TargetY = map.YAbsolute / 2;
-                while (!(x < 0 || y < 0 || x > map.XAbsolute || y > map.YAbsolute))
+                int x = CenterX, y = CenterY;
+                Enemy.TargetObject = new GameObject();
+                Enemy.TargetObject.X = CenterX;
+                Enemy.TargetObject.Y = CenterY;
+                while (x > CenterX - 900 && x < CenterX + 900 && y > CenterY - 600 && y < CenterY + 600)
                 {
-                    x = r.Next(-500, map.XAbsolute + 500);
-                    y = r.Next(-500, map.YAbsolute + 500);
+                    x = r.Next(0, CenterX * 2);
+                    y = r.Next(0, CenterY * 2);
                 }
                 Enemy.SetCoordinate(x, y);
-                Enemy.SetAngle((int)GameMath.GetAngleOfVector(map.XAbsolute / 2 - x, map.YAbsolute / 2 - y));
+                Enemy.SetAngle((int)GameMath.GetAngleOfVector(CenterX - x, CenterY - y));
                 map.ContainerSetMaxSide(Enemy.ContainerName, 72);
                 Enemy.Speed = 1;
                 obstacle.Add(Enemy);
@@ -183,6 +198,7 @@ namespace Tower_Defenc
                 Enemy.Range = 150;
                 Enemis.Add(Enemy);
                 Enemy.NeedToMove = true;
+                Enemy.NeedToRotate = true;
                 Enemy.SubdivisionNumber = 1;
                 /*
                   Баг "ХАКЕР"
@@ -235,36 +251,71 @@ namespace Tower_Defenc
         {
             if (SelectedUnit == null) return;
             if (SelectedUnit.HP == 0) return;
-            SelectedUnit.TargetX = x;
-            SelectedUnit.TargetY = y;
-            SelectedUnit.NeedToMove = true;
-            SelectedUnit.Speed = 1;
+            if (ClickCount > 0)
+            {
+                SelectedUnit.NeedToRotate = true;
+                if(map.Keyboard.IsKeyPressed(Key.Space))
+                {
+                    SelectedUnit.NeedToMove = true;
+                }
+                SelectedUnit.TargetObject = SelectedEnemyUnit;
+            }
+
+            else 
+            {
+                SelectedUnit.TargetObject = new GameObject();
+                SelectedUnit.TargetObject.X = x;
+                SelectedUnit.TargetObject.Y = y;
+                SelectedUnit.NeedToMove = true;
+                SelectedUnit.NeedToRotate = true;
+                SelectedUnit.Speed = 1;
+            }
         }
 
         void BuildUnit(string UnitName)
         {
-            if(!CanSpawn)
+            if (!CanSpawn)
             {
                 return;
             }
             counter++;
-            if (UnitName == "ЧИСТОТАНК")
-            {
-                if(money >=  50)
-                {
-                    AddMoney(-50);
-                    GameObject Tank = new GameObject("Tank_Low_AllY", "Tank_Low_ally" + counter.ToString(), "Tank_Low_ally", map.XAbsolute / 2, map.YAbsolute / 2, 60);
-                    CreatedAnimation.Add(Tank);
-                    Allies.Add(Tank);
-                    Tank.SetHp(100);
-                    Tank.SubdivisionNumber = 0;
-                    /*map.ContainerSetFrame(Tank.ContainerName + "Anime", "Fire Bolt1");
-                    map.ContainerSetMaxSide(Tank.ContainerName + "Anime", 100);
-                    map.AnimationStart(Tank.ContainerName + "Anime", "Fire1", -1);*/
-                    obstacle.Add(Tank);
-                }
+            switch (UnitName)
+            {   
+                case "ЧИСТОТАНК":
+                    if (money >= 50)
+                    {
+                        AddMoney(-50);
+                        GameObject Tank = new GameObject("Tank_Low_AllY", "Tank_Low_ally" + counter.ToString(), "Tank_Low_ally", CenterX, CenterY, 70);
+                        CreatedAnimation.Add(Tank);
+                        Allies.Add(Tank);
+                        Tank.SetHp(100);
+                        Tank.SubdivisionNumber = 0;
+                        /*map.ContainerSetFrame(Tank.ContainerName + "Anime", "Fire Bolt1");
+                        map.ContainerSetMaxSide(Tank.ContainerName + "Anime", 100);
+                        map.AnimationStart(Tank.ContainerName + "Anime", "Fire1", -1);*/
+                        obstacle.Add(Tank);
+                    }
+                    break;
+                case "ПУЛЕМЁТНИК":
+                    if(money >= 100)
+                    {
+                        AddMoney(-100);
+                        GameObject Tank = new GameObject("TankMashingan_Medium_ALLY", "TankMashingan_Medium_ALLY" + counter.ToString(), "TankMashingan_Medium_ALLY", CenterX, CenterY, 55);
+                        CreatedAnimation.Add(Tank);
+                        Tank.SetHp(120);
+                        Allies.Add(Tank);
+                        Tank.SubdivisionNumber = 0;
+                        obstacle.Add(Tank);
+                    }
+                    break;
+
+                /*case ""
+                    if (money >= 100)
+                    {
+
+                    }
+                    break;*/
             }
-            // ДЗ. Сделать вторую проверку на второй юнит
         }
 
         void AddMoney(int NewMoney)
@@ -293,10 +344,10 @@ namespace Tower_Defenc
         {
             for(int i = 0; i < Allies.Count; i++)
             {
-                if(Allies[i].X < map.XAbsolute / 2 &&
-                   Allies[i].X > map.XAbsolute / 2 - 150 &&
-                   Allies[i].Y < map.YAbsolute / 2 + 50 &&
-                   Allies[i].Y > map.YAbsolute / 2 - 50)
+                if(Allies[i].X < CenterX &&
+                   Allies[i].X > CenterX - 150 &&
+                   Allies[i].Y < CenterX + 50 &&
+                   Allies[i].Y > CenterY - 50)
                    
                 {
                     CanSpawn = false;
@@ -310,14 +361,19 @@ namespace Tower_Defenc
         {
             AnimatCreation();
             ChekSpawn();
-            for(int i = 0; i < Allies.Count; i++)
+            for (int i = 0; i < Allies.Count; i++)
             {
+                Allies[i].Rotate();
                 Allies[i].Move();
+                Allies[i].PerformAction();
             }
             for(int i = 0; i < Enemis.Count; i++)
             {
                 Enemis[i].Move();
+                Enemis[i].Rotate();
+                Enemis[i].CheckMaxCounter();
             }
+            ClickCount--;
         }
     }
 }
