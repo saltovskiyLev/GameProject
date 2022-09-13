14d127cc383e45f849a4f7fbe1307f1bdb964cbf
Лев <l_saltovskiy@mail.ru>
2022-09-13 19:42:50 +0300

    п

--- Квест2022/Квест 2022/Квест 2022/MainWindow.xaml.cs
+++ Квест2022/Квест 2022/Квест 2022/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
 using GameMaps;
+using System.Net.Http.Headers;
 
 namespace Квест_2022
 {
@@ -23,9 +24,12 @@ namespace Квест_2022
     public partial class MainWindow : Window
     {
         static public UniversalMap_Wpf map;
-        CellMapInfo mapInfo = new CellMapInfo(200, 200, 30, 0);
+        CellMapInfo mapInfo = new CellMapInfo(500, 500, 30, 0);
         GameObject Player;
         char[,] Cells;
+        int Level = 0;
+        List<string> Maps = new List<string>();
+        Dictionary<char, string> Scrolls = new Dictionary<char, string>();
         Dictionary<string, int> Items = new Dictionary<string, int>();
         public MainWindow()
         {
@@ -34,12 +38,46 @@ namespace Квест_2022
             MapContainer.Children.Add(map.Canvas);
             map.SetGridColor(Brushes.RosyBrown);
             //map.DrawGrid();
-            Cells = GetMap(@"C:\Users\Admin\Documents\GitHub\GameProject\Квест2022\Документация\карта.txt");
             AddPictures();
             map.Keyboard.SetSingleKeyEventHandler(CheckKey);
             GameObject.map = map;
-            DrawMap(Cells);
+            //Cells = GetMap(@"C:\GameProject-master\GameProject-master\Квест2022\Документация\карта.txt");
+            //DrawMap(Cells);
+            CreateMapList();
+            DrawNewMap(Maps[Level]);
         }
+
+        void CreateMapList()
+        {
+            Maps.Add(@"C:\GameProject-master\GameProject-master\Квест2022\Документация\001карта.txt");
+            Maps.Add(@"C:\GameProject-master\GameProject-master\Квест2022\Документация\002карта.txt");
+        }
+
+        void DrawNewMap(string FileName)
+        {
+            EraseMap();
+            Cells = GetMap(FileName);
+            DrawMap(Cells);
+            ReadScrolls(Level + 1);
+        }
+
+        void CheckViktory()
+        {
+            if (Cells[Player.X, Player.Y] == 'W')
+            {
+                Level++;
+                DrawNewMap(Maps[Level]);
+            }
+        }
+
+        /* Что нужно сделать чтобы перересовать карту для нового уровня?
+         1) Стереть старую карту
+         2) Получить из файла новую карту
+         3) Отрисовать новую карту
+
+         *** Что происходит с координатами игрока после перехода карты? ***
+         
+         */
         void AddPictures()
         {
             map.Library.ImagesFolder = new PathInfo { Path = "..\\..\\images", Type = PathType.Relative };
@@ -48,6 +86,8 @@ namespace Квест_2022
             map.Library.AddPicture("enemy", "enemy.png");
             map.Library.AddPicture("tree", "tree.png");
             map.Library.AddPicture("map", "map.jpg");
+            map.Library.AddPicture("gate_opened", "gate_opened.png");
+            map.Library.AddPicture("scroll", "i.png");
         }
         string[] GetLines(string FileName)
         {
@@ -57,6 +97,11 @@ namespace Квест_2022
                 Console.WriteLine(Lines[i]);
             }
             return Lines;
+        }
+
+        void EraseMap()
+        {
+            map.RemoveAllImagesInCells();
         }
 
         int GetMaxLength(string[] map)
@@ -72,102 +117,147 @@ namespace Квест_2022
             return max;
         }
 
+        void ReadScrolls(int MapNumber)
+        {
+            Scrolls.Clear();
+            string[] s = File.ReadAllLines(@"C:\GameProject-master\GameProject-master\Квест2022\Квест 2022\scrolls" + MapNumber + ".txt");
+            for(int i = 0; i < s.Length; i++)
+            {
+                string[] str = s[i].Split('|');
+                Scrolls.Add(str[0][0], str[1]);
+            }
+        }
+
+        string GetItemsText()
+        {
+            string Description = "";
+            foreach (string key in Items.Keys)
+            {
+                Description += key + ": " + Items[key] + " ";
+
+            }
+            return Description;
+        }
+
+
         char[,] GetMap(string FileName)
-        {
-            string[] MapLines = GetLines(FileName);
-            int MaxLength = GetMaxLength(MapLines);
-            char[,] map = new char[MaxLength, MapLines.Length];
-            for (int i = 0; i < MapLines.Length; i++)
             {
-                for (int j = 0; j < MapLines[i].Length; j++)
+                string[] MapLines = GetLines(FileName);
+                int MaxLength = GetMaxLength(MapLines);
+                char[,] map = new char[MaxLength, MapLines.Length];
+                for (int i = 0; i < MapLines.Length; i++)
                 {
-                    map[j, i] = MapLines[i][j];
-                }
-            }
-            return map;
-        }
-
-        void Collect(int x, int y)
-        {
-            string ItemName = "";
-            switch(Cells[x, y])
-            {
-                case '$':
-                    map.RemoveFromCell("money", x, y);
-                    ItemName = "coin";
-                    break;
-            }
-            if (ItemName == "") return;
-            if(Items.Keys.Contains(ItemName))
-            {
-                Items[ItemName]++;
-            }
-            else
-            {
-                Items.Add(ItemName, 1);
-            }
-        }
-
-        void DrawMap(char[,] cells)
-        {
-            for(int x = 0; x < cells.GetLength(0); x++)
-            {
-                for(int y = 0; y < cells.GetLength(1); y++)
-                {
-                    switch(cells[x, y])
+                    for (int j = 0; j < MapLines[i].Length; j++)
                     {
-                        case '1':
-                            map.DrawInCell("tree", x, y);
-                            break;
+                        map[j, i] = MapLines[i][j];
+                    }
+                }
+                return map;
+            }
 
-                        case '*':
-                            map.DrawInCell("enemy", x, y);
-                            break;
-                        case 'Y':
-                            Player = new GameObject("hero", x, y);
-                            break;
-                        case '$':
-                            map.DrawInCell("money", x, y);
-                            break;
+            void Collect(int x, int y)
+            {
+                string ItemName = "";
+                switch (Cells[x, y])
+                {
+                    case '$':
+                        map.RemoveFromCell("money", x, y);
+                        ItemName = "coin";
+                        break;
+
+                    case '!':
+                        map.RemoveFromCell("scroll", x, y);
+                    MessageBox.Show(Scrolls['!']);
+                        break;
+
+                }
+                if (ItemName == "") return;
+                if (Items.Keys.Contains(ItemName))
+                {
+                    Items[ItemName]++;
+                }
+                else
+                {
+                    Items.Add(ItemName, 1);
+                }
+                string Itemstext = GetItemsText();
+                tbItemText.Text = Itemstext;
+            }
+
+            void DrawMap(char[,] cells)
+            {
+                for (int x = 0; x < cells.GetLength(0); x++)
+                {
+                    for (int y = 0; y < cells.GetLength(1); y++)
+                    {
+                        switch (cells[x, y])
+                        {
+                            case '1':
+                                map.DrawInCell("tree", x, y);
+                                break;
+
+                            case '*':
+                                map.DrawInCell("enemy", x, y);
+                                break;
+
+                            case 'Y':
+                                Player = new GameObject("hero", x, y);
+                                break;
+
+                            case '$':
+                                map.DrawInCell("money", x, y);
+                                break;
+
+                            case 'W':
+                                map.DrawInCell("gate_opened", x, y);
+                                break;
+
+                            case '!':
+                                map.DrawInCell("scroll", x, y);
+                                break;
+                        }
                     }
                 }
             }
-        }
 
-        void CheckKey(Key k)
-        {
-            switch(k)
+            void CheckKey(Key k)
             {
-                case Key.W:
-                    if(CheckPassability(Player.X, Player.Y - 1)) Player.MoveTo(Player.X, Player.Y - 1);
-                    break;
+                switch (k)
+                {
+                    case Key.W:
+                        if (CheckPassability(Player.X, Player.Y - 1))
+                        {
+                            Player.MoveTo(Player.X, Player.Y - 1);
+                        }
+                        break;
 
-                case Key.S:
-                    if (CheckPassability(Player.X, Player.Y + 1)) Player.MoveTo(Player.X, Player.Y + 1);
-                    break;
+                    case Key.S:
+                        if (CheckPassability(Player.X, Player.Y + 1)) Player.MoveTo(Player.X, Player.Y + 1);
+                        break;
 
-                case Key.A:
-                    if (CheckPassability(Player.X - 1, Player.Y)) Player.MoveTo(Player.X - 1, Player.Y);
-                    break;
+                    case Key.A:
+                        if (CheckPassability(Player.X - 1, Player.Y)) Player.MoveTo(Player.X - 1, Player.Y);
+                        break;
 
-                case Key.D:
-                    if (CheckPassability(Player.X + 1, Player.Y)) Player.MoveTo(Player.X + 1, Player.Y);
+                    case Key.D:
+                        if (CheckPassability(Player.X + 1, Player.Y)) Player.MoveTo(Player.X + 1, Player.Y);
 
-                    break;
+                        break;
+                }
+                Collect(Player.X, Player.Y);
+                CheckViktory();
             }
-            Collect(Player.X, Player.Y);
-        }
 
-        bool CheckPassability(int x, int y)
-        {
-            if (Cells[x, y] == '1')
+            bool CheckPassability(int x, int y)
             {
-                return false;
-            }
-            else
-            {
-                return true;
+                if (Cells[x, y] == '1')
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
             }
         }
     }
-}
